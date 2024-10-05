1f932c46b27a8b9bbdd10fc93c10554961a1f60e
Ahmed Idrissi <83851898+ahmedidrissi@users.noreply.github.com>
2024-10-05 11:54:51 +0100

    Add farmerData module with initial farmer information

---
+++ src/data/farmerData.ts
@@ -0,0 +1,18 @@
+import { Farmer } from "@/types/farmer";
+
+const farmerData: Farmer = {
+  firstName: "Ahmed",
+  lastName: "Idrissi",
+  email: "iahmed@etayoga.ma",
+  personalPhoto: "/images/user/user-03.png",
+  farmName: "Tagoya",
+  farmCountry: "Morocco",
+  farmCity: "Rabat",
+  farmAddress: "Avenue Mohamed Ben Abdellah, Av. Regragui",
+  farmPostalCode: "10000",
+  farmLatitude: 34.020882,
+  farmLongitude: -6.84165,
+  xp: 500,
+};
+
+export default farmerData;
